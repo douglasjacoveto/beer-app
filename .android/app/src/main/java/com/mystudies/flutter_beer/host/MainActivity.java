7e9b3f1973bcecc55897b3ab355038bb7fd23fd6
Douglas Jacoveto <jdouglas@ciandt.com>
2020-11-05 14:48:38 -0300

    Commit Inicial

--- .android/app/src/main/java/com/mystudies/flutter_beer/host/MainActivity.java
+++ .android/app/src/main/java/com/mystudies/flutter_beer/host/MainActivity.java
@@ -1,17 +1,6 @@
 package com.mystudies.flutter_beer.host;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.view.FlutterMain;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    FlutterMain.startInitialization(this);
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-
 }
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # flutter_beer
-
 A new Flutter Beer module.
 
 ## Getting Started
-
 For help getting started with Flutter, view our online
 [documentation](https://flutter.dev/).
